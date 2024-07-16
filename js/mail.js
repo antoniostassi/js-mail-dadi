1bc26f4c2df6df00faf99f5439cefcacfc70200f
antoniostassi <stassiant@outlook.it>
2024-07-16 13:16:25 +0200

    JS | Logic Completed

--- js/mail.js
+++ js/mail.js
@@ -2,12 +2,27 @@ const adminEmails = [
     "mail@gmail.com",
     "mail2@gmail.com",
     "mail3@gmail.com",
-    "mai4l@gmail.com"
+    "mai4l@gmail.com",
+    "DevoStareMoltoCalmo@alessioscusa.com"
 ]
+let loggedIn = false;
 
+while(!loggedIn) {
 
-const usrLogin = prompt("Inserisci l'email per l'accesso.");
+    const usrLogin = prompt("Inserisci l'email per l'accesso.");
 
-for(let k = 0; k < adminEmails.lenght; k++){
+    for(let k = 0; k < adminEmails.length; k++){
+    
+        if (usrLogin == adminEmails[k]){
+            loggedIn = true;
+            alert("Sei entrato nel sistema.");
+        }
+    
+    }
+    
+    if (!loggedIn) {
+        alert("Hai inserito un'email errata.");
+    }
 
 }
+
